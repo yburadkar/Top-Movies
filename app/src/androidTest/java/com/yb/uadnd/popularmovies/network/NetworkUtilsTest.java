700a8fdfe2d97cac8fd827be835b6024f765e609
Yogesh Buradkar <yogesh.buradkar@gmail.com>
2019-05-23 23:36:12 +0100

    - Add Android Unit Tests for NetworkUtils
    Signed-off-by: Yogesh Buradkar <yogesh.buradkar@gmail.com>

---
+++ app/src/androidTest/java/com/yb/uadnd/popularmovies/network/NetworkUtilsTest.java
@@ -0,0 +1,45 @@
+package com.yb.uadnd.popularmovies.network;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.net.URL;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class NetworkUtilsTest {
+
+    @Test
+    public void checkBuildPosterUrl_forValidUrl(){
+        String imagePath = "ziEuG1essDuWuC5lpWUaw1uXY2O.jpg";
+        URL url = NetworkUtils.buildPosterURL(imagePath);
+        String actual = null;
+        if(url != null) actual = url.toString();
+        String expected = "https://image.tmdb.org/t/p/w342/" + imagePath;
+        assertEquals("Generate valid photo url failed: " + actual, expected, actual );
+    }
+
+    @Test
+    public void checkBuildPosterUrl_forNullImagePath(){
+        String imagePath = null;
+        URL url = NetworkUtils.buildPosterURL(imagePath);
+        String actual = null;
+        if(url != null) actual = url.toString();
+        String expected = null;
+        assertEquals("Failed for null image path: " + actual, expected, actual );
+    }
+
+    @Test
+    public void buildYouTubeUrl_forValidKey(){
+        String key = "ziEuG1essDu";
+        URL url = NetworkUtils.buildYouTubeUrl(key);
+        String actual = null;
+        if(url != null) actual = url.toString();
+        String expected = "https://www.youtube.com/watch?v="+ key;
+        assertEquals("Generate You tube url failed: " + actual, expected, actual );
+    }
+
+}
--- app/src/main/java/com/yb/uadnd/popularmovies/network/NetworkUtils.java
+++ app/src/main/java/com/yb/uadnd/popularmovies/network/NetworkUtils.java
@@ -8,12 +8,12 @@ import java.net.URL;
 public class NetworkUtils {
 
     //Url constants for getting movie posters
-    private static final String POSTERS_BASE_URL = "https://image.tmdb.org/t/p";
-    private static final String POSTER_SIZE_PATH = "w342";  //w185
+    public static final String POSTERS_BASE_URL = "https://image.tmdb.org/t/p";
+    public static final String POSTER_SIZE_PATH = "w342";  //w185
 
     //Url constant for Youtube videos
-    private static final String YOUTUBE_BASE_URL = "https://www.youtube.com/watch";
-    private static final String PARAM_VIDEO = "v";
+    public static final String YOUTUBE_BASE_URL = "https://www.youtube.com/watch";
+    public static final String PARAM_VIDEO = "v";
 
     /*Builds the url to retrieve the poster */
     public static URL buildPosterURL(String imagePath){
